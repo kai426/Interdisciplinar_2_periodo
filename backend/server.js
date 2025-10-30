716988d8e934a920b62fcd5b8a1a023ab280f048
Kaíky <kaiky2022@rede.ulbra.br>
2025-10-30 17:11:51 -0300

    feat: Configura CORS e URLs de API para deploy

--- backend/server.js
+++ backend/server.js
@@ -10,30 +10,32 @@ const agendamentoRoutes = require("./routes/agendamentoRoutes");
 
 const PORT = process.env.PORT || 3000;
 
-// Middlewares
-app.use(express.json());
+const whitelist = ['https://interdisciplinar-2-periodo.vercel.app']; 
 
-const whitelist = ['https://interdisciplinar-2-periodo.vercel.app/']; 
 const corsOptions = {
     origin: function (origin, callback) {
+        // Permite requisições da whitelist E requisições sem 'origin' (ex: Postman)
         if (whitelist.indexOf(origin) !== -1 || !origin) {
-            // Permite a URL da whitelist e requisições sem 'origin' (ex: Postman)
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
         }
-    }
+    },
+    optionsSuccessStatus: 200 // Para compatibilidade
 };
+
 app.use(cors(corsOptions));
+
+app.use(express.json());
 
 // Rotas
 app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/salas", salaRoutes);
 app.use("/api/agendamentos", agendamentoRoutes);
 
-// Endepoint de teste
+// Endpoint de teste
 app.get("/", (req, res) => {
-    res.send("API do Sistema de Agendamento de Salas está funcionando!")
+    res.send("API do Sistema de Agendamento de Salas está funcionando!")
 });
 
 // Iniciar o servidor
