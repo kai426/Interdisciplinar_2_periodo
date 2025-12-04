6ceaf8d2af17455ac2672fdc276f499b95d2e0e5
Kaíky <kaiky2022@rede.ulbra.br>
2025-12-04 18:11:09 -0300

    Simplify CORS configuration in server.js
    Removed CORS options and used default CORS middleware.

--- backend/server.js
+++ backend/server.js
@@ -10,21 +10,7 @@ const agendamentoRoutes = require("./routes/agendamentoRoutes");
 
 const PORT = process.env.PORT || 3000;
 
-const whitelist = ['https://interdisciplinar-2-periodo.vercel.app']; 
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        // Permite requisições da whitelist E requisições sem 'origin' (ex: Postman)
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200 // Para compatibilidade
-};
-
-app.use(cors(corsOptions));
+app.use(cors()); 
 
 app.use(express.json());
 
